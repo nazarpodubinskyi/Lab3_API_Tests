4157d659d29afaad838eeaf0952b33ee517f1e49
bezil <95500334+nazarpodubinskyi@users.noreply.github.com>
2022-11-22 10:17:05 +0200

    Add project files.

---
+++ Model/PostsModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RESTTest.Model
+{
+    public class BookingDates
+    {
+        public string checkin { get; set; }
+        public string checkout { get; set; }
+    }
+
+    public class PostsModel
+    {
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public int totalprice { get; set; }
+        public bool depositpaid { get; set; }
+        public BookingDates bookingdates { get; set; }
+        public string additionalneeds { get; set; }
+    }
+}
